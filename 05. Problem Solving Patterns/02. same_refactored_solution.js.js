e939e61d85635d0406b0a47f845f23154a577732
mh-saeed <49342183+mh-saeed@users.noreply.github.com>
2022-12-13 20:24:55 +0500

    updated

--- 05. Problem Solving Patterns/02. same_refactored_solution.js.js
+++ 05. Problem Solving Patterns/02. same_refactored_solution.js.js
@@ -1,6 +1,17 @@
-// write a function same that accepts two arrays.
-//  The function should return true if every value in the array has it's corresponding value squared in the second array.
-// The frequency(number of elements in each array) must be same
+/* 
+write a function same that accepts two arrays.
+The function should return true if every value 
+in the array has it's corresponding value squared 
+in the second array.
+The frequency(number of elements in each array) 
+must be same
+
+Expected behavior:
+same([1,2,3], [4,1,9]) // true
+same([1,2,3], [1,9]) // false
+same([1,2,1], [4,4,1]) // false (must be same frequency)
+*/
+
 
 function same(arr1, arr2) {
   if (arr1.length !== arr2.length) {
--- 05. Problem Solving Patterns/03. anagram.js
+++ 05. Problem Solving Patterns/03. anagram.js
@@ -2,7 +2,18 @@
 Given two strings, write a function to determine if the
 second string is an anagram of the first. 
 An anagram is a word, phrase, or name formed by rearranging the
-letters of another, such as cinema, formed from iceman. 
+letters of another, such as cinema, formed from iceman.
+
+Expected behavior:
+
+validAnagram('', '') // true
+validAnagram('aaz', 'zza') // false
+validAnagram('anagram', 'nagaram') // true
+validAnagram("rat","car")  // false
+validAnagram('awesome', 'awesom') // false
+validAnagram('qwerty', 'qeywrt') // true
+validAnagram('texttwisttime', 'timetwisttext') // true
+
 */
 
 function validAnagram(first, second) {
@@ -35,7 +46,7 @@ function validAnagram(first, second) {
 validAnagram('', '') // true
 validAnagram('aaz', 'zza') // false
 validAnagram('anagram', 'nagaram') // true
-validAnagram("rat","car") // false) // false
+validAnagram("rat","car")  // false
 validAnagram('awesome', 'awesom') // false
 validAnagram('qwerty', 'qeywrt') // true
 validAnagram('texttwisttime', 'timetwisttext') // true
