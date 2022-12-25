8736da3f607e418a0157832b5a339204932c560b
mh-saeed <49342183+mh-saeed@users.noreply.github.com>
2022-12-25 13:56:50 +0500

    Updated: areThereDuplicatesLinearSolution

--- 03 Problem Solving Patterns/2.2 Multiple Pointers Pattern/05 areThereDuplicatesLinearSolution.js
+++ 03 Problem Solving Patterns/2.2 Multiple Pointers Pattern/05 areThereDuplicatesLinearSolution.js
@@ -1,3 +1,7 @@
+// Multiple Pointers - areThereDuplicates
+// Implement a function called, areThereDuplicates which accepts a variable number of arguments,
+// and checks whether there are any duplicates among the arguments passed in.
+
 function areThereDuplicatesLinearSolution() {
   return new Set(arguments).size !== arguments.length;
 }
