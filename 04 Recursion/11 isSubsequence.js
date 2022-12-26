f05f527055d24a3571b16641c2ce678ae75c543f
mh-saeed <49342183+mh-saeed@users.noreply.github.com>
2022-12-26 18:30:07 +0500

    added: Recursive isSubsequence Solution

---
+++ 04 Recursion/11 isSubsequence.js
@@ -0,0 +1,21 @@
+// Multiple Pointers - isSubsequence
+// Write a function called isSubsequence which takes in two strings and checks
+// whether the characters in the first string form a subsequence of the characters
+// in the second string.
+
+// In other words, the function should check whether the characters
+// in the first string appear somewhere in the second string, without their order changing.
+
+// isSubsequence Solution - Recursive but not O(1) Space
+
+function isSubsequence(str1, str2) {
+  if (str1.length === 0) return true;
+  if (str2.length === 0) return false;
+  if (str2[0] === str1[0]) return isSubsequence(str1.slice(1), str2.slice(1));
+  return isSubsequence(str1, str2.slice(1));
+}
+
+console.log(isSubsequence("sing", "sting")); // true
+console.log(isSubsequence("abc", "abracadabra")); // true
+console.log(isSubsequence("abc", "acb")); // false (order matters)
+console.log(isSubsequence("", "cat")); // true
