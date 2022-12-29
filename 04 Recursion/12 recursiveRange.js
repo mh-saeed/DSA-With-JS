8e0c88db479189b8c15e44a60e7f44711e72da0b
mh-saeed <49342183+mh-saeed@users.noreply.github.com>
2022-12-29 12:27:38 +0500

    added: recursiveRange

---
+++ 04 Recursion/12 recursiveRange.js
@@ -0,0 +1,11 @@
+// recursiveRange
+// Write a function called recursiveRange which accepts a number
+// and adds up all the numbers from 0 to the number passed to the function.
+
+function recursiveRange(num) {
+  if (num <= 0) return 0;
+  return num + recursiveRange(--num);
+}
+
+console.log(recursiveRange(6)); // 21
+console.log(recursiveRange(10)); // 55
