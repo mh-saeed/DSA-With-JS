89a91580a744504bc9f7b2db223ccddafd86d409
mh-saeed <49342183+mh-saeed@users.noreply.github.com>
2023-01-15 16:43:14 +0500

    Updated

--- 06 Sorting Algorithms/6.2 Bubble Sort/02 bubbleSortUnoptimized.js.js
+++ 06 Sorting Algorithms/6.2 Bubble Sort/02 bubbleSortUnoptimized.js.js
@@ -15,6 +15,8 @@ function bubbleSortES5(arr) {
   return arr;
 }
 
+console.log(bubbleSortES5([8, 1, 2, 3, 4, 5, 6, 7]));
+
 // ES2015 Version
 function bubbleSortES2015(arr) {
   const swap = (arr, idx1, idx2) => {
@@ -32,4 +34,3 @@ function bubbleSortES2015(arr) {
 }
 
 console.log(bubbleSortES2015([8, 1, 2, 3, 4, 5, 6, 7]));
-console.log(bubbleSortES5([8, 1, 2, 3, 4, 5, 6, 7]));
