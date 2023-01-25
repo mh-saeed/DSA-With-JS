b1d585b3544b57392b06c2b1567e5b959f2ccea5
mh-saeed <49342183+mh-saeed@users.noreply.github.com>
2023-01-25 23:39:19 +0500

    updated

--- 01 Big O Notation/05 logAtLeast5.js
+++ 01 Big O Notation/05 logAtLeast5.js
@@ -1,7 +1,12 @@
+// what is Time Complexity of the below code ?
+// Answer with explanation is given below at the end
+
 function logAtLeast5(n = 0) {
   for (var i = 1; i <= Math.max(5, n); i++) {
     console.log(i);
   }
 }
 
-logAtLeast5(10); // Time Complexity O(n)
+logAtLeast5(10);
+
+// Time Complexity = O(n)
