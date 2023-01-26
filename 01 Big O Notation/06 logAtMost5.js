dbbb07e0fa6b80b72c831ec9297cf559a0af1efb
mh-saeed <49342183+mh-saeed@users.noreply.github.com>
2023-01-26 07:32:03 +0500

    updated

--- 01 Big O Notation/06 logAtMost5.js
+++
@@ -1,6 +0,0 @@
-function logAtMost5(n = 0) {
-  for (var i = 1; i <= Math.min(5, n); i++) {
-    console.log(i);
-  }
-}
-logAtMost5(10); // Time Complexity O(1)
--- README.md
+++
@@ -1,1 +0,0 @@
-# JavaScript-Algorithms-and-Data-Structures
