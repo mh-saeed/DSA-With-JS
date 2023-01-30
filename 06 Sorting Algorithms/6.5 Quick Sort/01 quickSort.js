8c3d74d5741e379a10338552e05614464281776f
mh-saeed <49342183+mh-saeed@users.noreply.github.com>
2023-01-30 13:13:50 +0500

    added: quickSort

--- 06 Sorting Algorithms/6.5 Quick Sort/01 quickSort.js
+++ 06 Sorting Algorithms/6.5 Quick Sort/01 quickSort.js
@@ -19,4 +19,15 @@ function pivot(arr, start = 0, end = arr.length - 1) {
   return swapIdx;
 }
 
-pivot([4, 8, 2, 1, 5, 7, 6, 3]);
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(arr, left, right); //3
+    //left
+    quickSort(arr, left, pivotIndex - 1);
+    //right
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+}
+
+console.log(quickSort([100, -3, 2, 4, 6, 9, 1, 2, 5, 3, 23]));
