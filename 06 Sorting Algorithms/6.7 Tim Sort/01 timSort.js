2fe1fba9f8010101b93aa781c11f8d80f0873e2f
mh-saeed <49342183+mh-saeed@users.noreply.github.com>
2023-02-01 01:44:15 +0500

    updated: timSort Algo

--- 06 Sorting Algorithms/6.7 Tim Sort/01 timSort.js
+++ 06 Sorting Algorithms/6.7 Tim Sort/01 timSort.js
@@ -1,8 +1,8 @@
 // Function to merge two sorted arrays into a single sorted array
 function mergeSortedArrays(leftArray, rightArray) {
-  let result = [];
-  let leftIndex = 0;
-  let rightIndex = 0;
+  let result = []; // Array to hold the result of merging the two input arrays
+  let leftIndex = 0; // Index of the current element in the left array
+  let rightIndex = 0; // Index of the current element in the right array
 
   // Compare elements from both arrays and push the smaller one to the result array
   while (leftIndex < leftArray.length && rightIndex < rightArray.length) {
@@ -30,8 +30,43 @@ function mergeSortedArrays(leftArray, rightArray) {
   return result;
 }
 
-// Timsort function to sort an array
-function timsort(array) {
+// Recursive function to merge the sorted runs until there's only one remaining run
+function mergeRuns(runList) {
+  if (runList.length === 1) {
+    return runList[0];
+  }
+
+  let newRunList = []; // List of merged runs
+  for (let i = 0; i < runList.length; i += 2) {
+    if (i + 1 < runList.length) {
+      newRunList.push(mergeSortedArrays(runList[i], runList[i + 1]));
+    } else {
+      newRunList.push(runList[i]);
+    }
+  }
+  return mergeRuns(newRunList);
+}
+
+// ------- Timsort function to sort an array - (With Recursion) -------
+
+function timSortRec(array) {
+  let runList = []; // List of sorted runs to be merged
+  let runSize = 32; // Size of each run
+
+  // Split the array into runs of size runSize and sort each run using built-in sort method
+  for (let startIndex = 0; startIndex < array.length; startIndex += runSize) {
+    let endIndex = Math.min(startIndex + runSize, array.length); // End index of the current run
+    let run = array.slice(startIndex, endIndex); // The current run
+    run.sort((a, b) => a - b); // Sort the current run using built-in sort method
+    runList.push(run); // Add the current run to the list of sorted runs
+  }
+
+  return mergeRuns(runList);
+}
+
+// ------- Timsort function to sort an array - (Non Recursion Way) -------
+
+function timSortNonRec(array) {
   let runList = [];
   let runSize = 32;
 
@@ -59,4 +94,5 @@ function timsort(array) {
   return runList[0];
 }
 
-console.log(timsort([100, -3, 2, 4, 6, 9, 1, 2, 5, 3, 23, -100, 55]));
+console.log(timSortRec([100, -3, 2, 4, 6, 9, 1, 2, 5, 3, 23, -100, 55]));
+console.log(timSortNonRec([100, -3, 2, 4, 6, 9, 1, 2, 5, 3, 23, -100, 55]));
