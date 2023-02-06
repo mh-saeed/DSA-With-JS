6f310495865ba3fc69e5023a3b05de69568ab7c7
mh-saeed <49342183+mh-saeed@users.noreply.github.com>
2023-02-06 22:49:24 +0500

    added: SinglyList(Push Method)

---
+++ 07 Linked List/ 01 linkedList.js
@@ -0,0 +1,35 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(value) {
+    var newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+
+    this.length++;
+
+    return this;
+  }
+}
+
+var list = new SinglyLinkedList();
+
+console.log(list.push("HELLO"));
+console.log(list.push("GOODBYE"));
