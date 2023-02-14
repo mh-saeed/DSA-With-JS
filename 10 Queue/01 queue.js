20b9a42284a96aca6f6c6c39cb5385a17086a55c
mh-saeed <49342183+mh-saeed@users.noreply.github.com>
2023-02-15 00:38:20 +0500

    added: queue.js

--- 10 Queue/01 queue.js
+++ 10 Queue/01 queue.js
@@ -5,30 +5,27 @@ class Node {
   }
 }
 
-class Stack {
+class Queue {
   constructor() {
     this.first = null;
     this.last = null;
     this.size = 0;
   }
-
-  // Add a value to the top of the stack!
-  push(val) {
+  enqueue(val) {
     var newNode = new Node(val);
     if (!this.first) {
       this.first = newNode;
       this.last = newNode;
     } else {
-      var temp = this.first;
-      this.first = newNode;
-      this.first.next = temp;
+      this.last.next = newNode;
+      this.last = newNode;
     }
     return ++this.size;
   }
 
-  // Remove a value from the top of the stack!
-  pop() {
+  dequeue() {
     if (!this.first) return null;
+
     var temp = this.first;
     if (this.first === this.last) {
       this.last = null;
@@ -39,14 +36,14 @@ class Stack {
   }
 }
 
-let stack = new Stack();
+let queue = new Queue();
 
-console.log(stack.push("Start"));
-console.log(stack.push("0"));
-console.log(stack.push("1"));
-console.log(stack.push("2"));
-console.log(stack.push("3"));
-console.log(stack.push("end"));
+console.log(queue.enqueue("Start"));
+console.log(queue.enqueue("0"));
+console.log(queue.enqueue("1"));
+console.log(queue.enqueue("2"));
+console.log(queue.enqueue("3"));
+console.log(queue.enqueue("end"));
 
-console.log(stack.pop()); // end
-console.log(stack.pop()); // 3
+console.log(queue.dequeue()); // start
+console.log(queue.dequeue()); // 0
